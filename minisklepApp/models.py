b6411a4eb9ae70eed27ebae1cfe73b7bf7edb756
JakubOlejnik2007 <jacobole2000@gmail.com>
2025-02-26 12:58:25 +0100

    Migrations with db models

--- minisklepApp/models.py
+++ minisklepApp/models.py
@@ -6,6 +6,6 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     img = models.ImageField(upload_to="products/", null=True, blank=True, default=None)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
